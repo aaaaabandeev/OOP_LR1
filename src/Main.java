fecb394e5bd7f64096ddeaf6c3845690f03a937f
aaaaabandeev <125127673+aaaaabandeev@users.noreply.github.com>
2023-10-04 22:04:11 +0600

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        //Ввод текста
+        System.out.print("Введите текст: ");
+        String text = scanner.nextLine();
+        int k = 0;
+        while ( true ) // ввод числа строк
+        {
+            System.out.print("Введите значение k: ");
+            Scanner sc1 = new Scanner(System. in );
+            try
+            {
+                k = sc1.nextInt();
+                break;
+            }
+            catch(InputMismatchException fg)
+            {
+                // если введенное значение не является числом
+                System.out.print("Вы ввели не число. " );
+            }
+        }
+
+        // Введите символ, которым нужно заменить k-ю букву
+        System.out.print("Введите символ для замены: ");
+        char replacementChar = scanner.next().charAt(0);
+
+        // Разделим текст на слова и обработаем каждое слово
+        String[] words = text.split("\\s+"); // Разделение текста по пробелам
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            if (k <= word.length()) {
+                // Если k не превышает длину слова, замена символа
+                char[] charArray = word.toCharArray();
+                charArray[k - 1] = replacementChar;
+                result.append(new String(charArray)).append(" ");
+            } else {
+                // Нет - просто добавление слова
+                result.append(word).append(" ");
+            }
+        }
+        // Корректировка
+        String finalResult = result.toString().trim();
+        System.out.println("Результат: " + finalResult);
+    }
+}
